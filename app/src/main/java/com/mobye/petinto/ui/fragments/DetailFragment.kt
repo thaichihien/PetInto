b3d7594284ab6194105b89deac40b6a7227f934a
thaichihien <79949235+thaichihien@users.noreply.github.com>
2023-03-20 18:25:29 +0700

    UI for Detail, Cart

---
+++ app/src/main/java/com/mobye/petinto/ui/fragments/DetailFragment.kt
@@ -0,0 +1,14 @@
+package com.mobye.petinto.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mobye.petinto.R
+
+
+class DetailFragment : Fragment(R.layout.fragment_detail) {
+
+
+}
