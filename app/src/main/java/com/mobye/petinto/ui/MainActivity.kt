696093abc0955fc5c2f079c9b1b45043572b0293
thaichihien <79949235+thaichihien@users.noreply.github.com>
2023-04-08 12:15:03 +0700

    Push notification

--- app/src/main/java/com/mobye/petinto/ui/MainActivity.kt
+++ app/src/main/java/com/mobye/petinto/ui/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.mobye.petinto.ui
 
+import android.Manifest
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Context
+import android.content.pm.PackageManager
 import android.graphics.Color
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
@@ -14,18 +16,22 @@ import android.view.Window
 import android.view.WindowManager
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.datastore.preferences.preferencesDataStore
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.FirebaseMessaging
 import com.mobye.petinto.R
 import com.mobye.petinto.databinding.ActivityMainBinding
 import com.mobye.petinto.models.Customer
@@ -53,6 +59,8 @@ fun Dialog.changeToSuccess(message : String) {
     tvMessage.text = message
 }
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
 
     private lateinit var navController: NavController
     lateinit var bottomNavView : BottomNavigationView
@@ -97,6 +105,7 @@ class MainActivity : AppCompatActivity() {
 //        }
 
 
+
         if(firebaseAuth.currentUser != null){
             // check where user from
 
@@ -108,15 +117,56 @@ class MainActivity : AppCompatActivity() {
 
             Log.d("LOGIN_ACCOUNT","Profile : ${firebaseAuth.currentUser!!.email} | ${firebaseAuth.currentUser!!.displayName}")
             informationViewModel.getUser(firebaseAuth.uid!!)
+
+            FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener {
+                if(it.isSuccessful){
+                    val token = it.result
+                    Log.e(TAG,"token : $token")
+                }else{
+                    Log.e(TAG,"token : FAIL")
+                }
+            })
+
+
         }
 
-
+        askNotificationPermission()
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
         navController = navHostFragment.navController
         bottomNavView = binding.bottomNavView
         setupWithNavController(bottomNavView, navController)
 
+    }
+
+    // Declare the launcher at the top of your Activity/Fragment:
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // FCM SDK (and your app) can post notifications.
+        } else {
+            // TODO: Inform user that that your app will not show notifications.
+        }
+    }
+
+    private fun askNotificationPermission() {
+        // This is only necessary for API level >= 33 (TIRAMISU)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                // FCM SDK (and your app) can post notifications.
+            } else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                // TODO: display an educational UI explaining to the user the features that will be enabled
+                //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
+                //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
+                //       If the user selects "No thanks," allow the user to continue without notifications.
+            } else {
+                // Directly ask for the permission
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
     }
 
     fun showBottomNav(){
