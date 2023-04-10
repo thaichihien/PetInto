80e09af2fd763a23238d1f557379e9711b22bdbc
thaichihien <79949235+thaichihien@users.noreply.github.com>
2023-04-10 09:18:28 +0700

    config

--- app/src/main/java/com/mobye/petinto/utils/Secret.kt
+++
@@ -1,7 +0,0 @@
-package com.mobye.petinto.utils
-
-object Secret {
-    const val BASE_URL = "https://petinto-1-n9143861.deta.app/"
-    const val DELIVERY_FEE = 30000
-    const val WEB_CLIENT = "566087222915-6eg3dse3jsg7dpu8afspordeujgkljou.apps.googleusercontent.com"
-}
