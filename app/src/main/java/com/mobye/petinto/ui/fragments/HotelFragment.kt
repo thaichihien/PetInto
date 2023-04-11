5accfa903646f1248690e3dfba472f7728190526
Nizo <nguyenduynien11102002@gmail.com>
2023-04-11 20:51:36 +0700

    fragment_service.xml

--- app/src/main/java/com/mobye/petinto/ui/fragments/HotelFragment.kt
+++ app/src/main/java/com/mobye/petinto/ui/fragments/HotelFragment.kt
@@ -1,9 +1,25 @@
 package com.mobye.petinto.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.mobye.petinto.R
+import com.mobye.petinto.databinding.FragmentHotelBinding
+import com.mobye.petinto.databinding.FragmentShoppingBinding
+import com.mobye.petinto.repository.ShoppingRepository
+import com.mobye.petinto.viewmodels.ShoppingViewModel
+import com.mobye.petinto.viewmodels.ShoppingViewModelFactory
 
 
 class HotelFragment : Fragment(R.layout.fragment_hotel) {
+
+    val DEBUG_TAG = "HotelFragment"
+    private var _binding : FragmentHotelBinding? = null
+    private val binding get() = _binding!!
+
+
 
 }
