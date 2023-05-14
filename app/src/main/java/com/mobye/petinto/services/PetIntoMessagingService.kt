3d9104354cf491369fe41e82b7a3ac419e2529a5
_april.inme <dung8ahxh@gmail.com>
2023-05-14 10:12:47 +0700

    Test and Clean code Services

--- app/src/main/java/com/mobye/petinto/services/PetIntoMessagingService.kt
+++ app/src/main/java/com/mobye/petinto/services/PetIntoMessagingService.kt
@@ -22,10 +22,7 @@ import com.mobye.petinto.utils.Utils
 class PetIntoMessagingService : FirebaseMessagingService() {
 
     private val TAG = "PetIntoMessagingService"
-
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(TAG, "From: ${remoteMessage.from}")
 
         // Check if message contains a data payload.
@@ -36,42 +33,14 @@ class PetIntoMessagingService : FirebaseMessagingService() {
             val title = remoteMessage.data["title"]
             val type = remoteMessage.data["type"]
 
-
-
             val notification = Notification(
                 title!!,body!!,type!!,
                 Utils.getCurrentTimeString()
             )
-
             NotificationDatabase.createNotification(notification)
 
-
             showNotification(title!!,body!!,type!!)
-
-//            if (/* Check if data needs to be processed by long running job */ true) {
-//                // For long-running tasks (10 seconds or more) use WorkManager.
-//                scheduleJob()
-//            } else {
-//                // Handle message within 10 seconds
-//                handleNow()
-//            }
         }
-
-        // Check if message contains a notification payload.
-//        remoteMessage.notification?.let {
-//            Log.d(TAG, "Message Notification Body: ${it.body}")
-//
-//            val body = remoteMessage.notification!!.body
-//            val title = remoteMessage.notification!!.title
-//
-//            showNotification(title!!,body!!)
-//
-//        }
-
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
-
-
     }
 
     private fun showNotification(title: String, body: String,type : String) {
@@ -100,22 +69,13 @@ class PetIntoMessagingService : FirebaseMessagingService() {
             .setAutoCancel(true)
 
         if(type == "BOOKING"){
-//            val profileIntent = Intent(this,MainActivity::class.java)
-//            profileIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-//            profileIntent.putExtra("fragment","report")
-//            val pendingIntent = PendingIntent.getActivity(this,0,
-//                profileIntent,PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE)
-
             val pendingIntent = NavDeepLinkBuilder(this)
                 .setGraph(R.navigation.nav_graph)
                 .setDestination(R.id.bookingFragment)
                 .setComponentName(MainActivity::class.java)
                 .createPendingIntent()
 
-
             builder.setContentIntent(pendingIntent)
-
-
         }
 
 
@@ -125,26 +85,13 @@ class PetIntoMessagingService : FirebaseMessagingService() {
                     Manifest.permission.POST_NOTIFICATIONS
                 ) != PackageManager.PERMISSION_GRANTED
             ) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
                 Log.e(TAG,"Permission denied")
                 return
             }
             notify(notificationID,builder.build())
         }
     }
-
-    
-
     override fun onNewToken(token: String) {
         super.onNewToken(token)
-
     }
-
-
 }
